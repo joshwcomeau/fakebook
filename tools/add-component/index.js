166d8074f3d91b17300892e7d72dce27702359f6
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-09 18:57:26 -0400

    Install dependencies, create add-component tool

---
+++ tools/add-component/index.js
@@ -0,0 +1,121 @@
+/* eslint-disable no-console, no-use-before-define */
+const path = require('path');
+const fs = require('fs');
+const changeCase = require('change-case');
+
+
+function run(ComponentName) {
+  if (!ComponentName) {
+    throw new Error(`
+      Please supply a component name!
+      'npm run add-component -- YourComponentName'
+    `);
+  } else if (!changeCase.isUpperCase(ComponentName[0])) {
+    throw new Error(`
+      Custom React components need to be in PascalCase.
+      You provided ${ComponentName}.
+
+      Please capitalize the first letter:
+      "${changeCase.upperCaseFirst(ComponentName)}"
+    `);
+  }
+
+  const componentDirectory = path.join(
+    __dirname,
+    '../../src/components',
+    ComponentName
+  );
+  createDirectory(componentDirectory);
+
+  const className = changeCase.paramCase(ComponentName);
+
+  // Create and write JS to file
+  const componentPath = path.join(componentDirectory, 'index.js');
+  const componentTemplate = buildJSTemplate(ComponentName, className);
+  fs.writeFileSync(componentPath, componentTemplate);
+
+  // Create and write SCSS to file
+  const lessPath = path.join(componentDirectory, 'index.scss');
+  const lessTemplate = buildSCSSTemplate(ComponentName, className);
+  fs.writeFileSync(lessPath, lessTemplate);
+
+  // Create and write a test to file
+  const testPath = path.join(
+    componentDirectory,
+    `index.test.js`
+  );
+  const testTemplate = buildTestTemplate(ComponentName);
+  fs.writeFileSync(testPath, testTemplate);
+
+  console.info(`Component ${ComponentName} successfully created!`);
+  return true;
+}
+
+
+// Helper Methods
+function createDirectory(componentDirectory) {
+  try {
+    fs.mkdirSync(componentDirectory);
+  } catch (err) {
+    throw new Error(`Sorry, it appears the component ${ComponentName} already exists!`);
+  }
+
+  return componentDirectory;
+}
+
+function buildJSTemplate(ComponentName, className) {
+  // Not digging the break in indentation here,
+  // but it's needed for the file to render correctly :(
+  return `\
+// eslint-disable-next-line no-unused-vars
+import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
+
+import './index.css';
+
+
+const ${ComponentName} = () => {
+  const classes = classNames('${className}');
+
+  return (
+    <div className={classes}>
+      Your Component Here :)
+    </div>
+  );
+};
+
+${ComponentName}.propTypes = {
+
+};
+
+${ComponentName}.defaultProps = {
+
+};
+
+export default ${ComponentName};\n`;
+}
+
+function buildSCSSTemplate(ComponentName, className) {
+  return `\
+.${className} {
+
+}\n`;
+}
+
+function buildTestTemplate(ComponentName) {
+  return `\
+/* eslint-disable no-undef */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ${ComponentName} from './index';
+
+describe('${ComponentName}', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<${ComponentName} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});\n`;
+}
+
+const ComponentName = process.argv[2];
+run(ComponentName);
